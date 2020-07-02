35565d223e33157d389c56baa941b7c5431c3301
NguyetMoon <nguyetmoon1804992gmail.com>
2020-07-03 00:22:21 +0700

    edit phase

--- MisaWebApi/Controllers/PhaseController.cs
+++ MisaWebApi/Controllers/PhaseController.cs
@@ -154,7 +154,69 @@ namespace MisaWebApi.Controllers
             await _context.SaveChangesAsync();
             return item;
         }
+        // PUT: api/Phase/edit
+        [HttpPut("editPhase")]
+        public async Task<ActionResult<Phase>> PutPhase([FromBody] Phase model)
+        {
 
+            var item = await _context.Phase.FirstOrDefaultAsync(x => x.Id.Equals(model.Id));
+
+            if (item == null) return NotFound();
+
+            item.PhaseName = model.PhaseName;
+            item.Icon = model.Icon;
+            item.Description = model.Description;
+            item.LimitUser = model.LimitUser;
+
+            foreach (var a in model.FieldData)
+            {
+                // tim cai field id nay da ton tai chua
+                var fieldItem = await _context.FieldData.FindAsync(a.Id);
+                // kiem tra da ton tai thi update 
+                if (fieldItem != null)
+                {
+                    // update
+                    fieldItem.FieldName = a.FieldName;
+                    fieldItem.Description = a.Description;
+                    fieldItem.Type = a.Type;
+                    fieldItem.Required = a.Required;
+                    foreach(var b in a.Option)
+                    {
+                        var optionItem = await _context.Option.FindAsync(b.Id);
+                        if(optionItem != null)
+                        {
+                            optionItem.Value = b.Value;
+                        } else
+                        {
+                            var newOption = new Models.Option
+                            {
+                                Id = b.Id,
+                                Value = b.Value,
+                                FieldDataId = b.FieldDataId
+                            };
+                            _context.Option.AddRange(newOption);
+                        }
+                    }
+                }
+                else
+                {
+                    // create new
+                    var newField = new FieldData
+                    {
+                        Id = a.Id,
+                        FieldName = a.FieldName,
+                        Description = a.Description,
+                        Type = a.Type,
+                        Required = a.Required,
+                        PhaseId = a.PhaseId,
+                    };
+                    _context.FieldData.AddRange(newField);
+                }
+            }
+
+                await _context.SaveChangesAsync();
+            return item;
+        }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
